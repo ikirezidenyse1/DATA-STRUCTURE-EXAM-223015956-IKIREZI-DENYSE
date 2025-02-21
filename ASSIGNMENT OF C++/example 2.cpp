318db996124ceae098762609220837c6c7b4c09e
ikirezidenyse1 <ikirezidenyse@gmail.com>
2025-02-21 10:35:13 +0200

    Add files via upload

---
+++ ASSIGNMENT OF C++/example 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	    int age = 20;  // Integer variable
+    double price = 19.99;  // Double variable
+    char grade = 'A';  // Character variable
+    string name = "Alice";  // String variable
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Price: $" << price << endl;
+    cout << "Grade: " << grade << endl;
+    
+
+	return 0;
+}
